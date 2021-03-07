2ce35d9751e854d3ae6ab24089c76b721af83a4e
ShashankR87 <shashankperso@gmail.com>
2021-03-07 12:43:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
-import pandas as pd
+from flask import Flask, render_template, request, redirect, url_for, session, flash, send_file
 import numpy as np
+import pandas as pd
+from datetime import datetime
 import cv2
 import os
 import re
@@ -21,10 +22,31 @@ from torch.utils.data import DataLoader, Dataset
 from torch.utils.data.sampler import SequentialSampler
 
 from matplotlib import pyplot as plt
-test_df = pd.read_csv('./sample-train.csv')
-DIR_TEST = './test'
+app = Flask(__name__)
 
-WEIGHTS_FILE = './fasterrcnn_resnet50_fpn.pth'
+app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5rcb'
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    if request.method == 'POST':
+
+        userFile = request.files["userFile"]
+        curtime = str(datetime.now())
+        filename = 'static/img/usrFile' + curtime + '.jpg'
+        test_filename = 'test/usrFile' + curtime + '.jpg'
+        htmlfilename = 'static/img/usrFile' + curtime + '.jpg'
+        userFile.save(filename)
+        userFile.save(test_filename)
+        session["fileLoc"] = filename
+        processedFileLoc = processFiles(test_filename)
+        return render_template('index.html',processed='yes',uploaded=htmlfilename,result=processedFileLoc)
+
+    return render_template('index.html',processed='no',uploaded='',result='')
+
+test_df = pd.read_csv('sample-train.csv')
+DIR_TEST = 'test'
+
+WEIGHTS_FILE = 'fasterrcnn_resnet50_fpn.pth'
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
 
 class TomatoTestDataset(Dataset):
@@ -64,36 +86,6 @@ def get_test_transform():
         ToTensorV2(p=1.0)
     ])
 
-device = torch.device('cpu')
-
-num_classes = 2  # 1 class (tomato) + background
-
-# get number of input features for the classifier
-in_features = model.roi_heads.box_predictor.cls_score.in_features
-
-# replace the pre-trained head with a new one
-model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
-# Load the trained weights
-model.load_state_dict(torch.load(WEIGHTS_FILE,map_location=torch.device('cpu')))
-model.eval()
-
-x = model.to(device)
-
-def collate_fn(batch):
-    return tuple(zip(*batch))
-
-test_dataset = TomatoTestDataset(test_df, DIR_TEST, get_test_transform())
-
-test_data_loader = DataLoader(
-    test_dataset,
-    batch_size=4,
-    shuffle=False,
-    num_workers=4,
-    drop_last=False,
-    collate_fn=collate_fn
-)
-
 def format_prediction_string(boxes, scores):
     pred_strings = []
     for j in zip(scores, boxes):
@@ -101,47 +93,70 @@ def format_prediction_string(boxes, scores):
 
     return " ".join(pred_strings)
 
+def processFiles(filename):
 
-detection_threshold = 0.5
-results = []
-images = []
-outputs = []
-for images, image_ids in test_data_loader:
+    test_df = test_df.append({'image_id': filename, 'PredictionString': '1.0 0 0 50 50'})
+    #do shit
+    test_dataset = TomatoTestDataset(test_df, DIR_TEST, get_test_transform())
 
-    images = list(image.to(device) for image in images)
-    outputs = model(images)
+    test_data_loader = DataLoader(
+        test_dataset,
+        batch_size=4,
+        shuffle=False,
+        num_workers=4,
+        drop_last=False,
+        collate_fn=collate_fn
+    )
+    detection_threshold = 0.5
+    results = []
+    images = []
+    outputs = []
+    for images, image_ids in test_data_loader:
 
-    for i, image in enumerate(images):
-        boxes = outputs[i]['boxes'].data.cpu().numpy()
-        scores = outputs[i]['scores'].data.cpu().numpy()
+        images = list(image.to(device) for image in images)
+        outputs = model(images)
 
-        boxes = boxes[scores >= detection_threshold].astype(np.int32)
-        scores = scores[scores >= detection_threshold]
-        image_id = image_ids[i]
+        for i, image in enumerate(images):
+            boxes = outputs[i]['boxes'].data.cpu().numpy()
+            scores = outputs[i]['scores'].data.cpu().numpy()
 
-        boxes[:, 2] = boxes[:, 2] - boxes[:, 0]
-        boxes[:, 3] = boxes[:, 3] - boxes[:, 1]
+            boxes = boxes[scores >= detection_threshold].astype(np.int32)
+            scores = scores[scores >= detection_threshold]
+            image_id = image_ids[i]
 
-        result = {
-            'image_id': image_id,
-            'PredictionString': format_prediction_string(boxes, scores)
-        }
+            boxes[:, 2] = boxes[:, 2] - boxes[:, 0]
+            boxes[:, 3] = boxes[:, 3] - boxes[:, 1]
 
-        results.append(result)
+            result = {
+                'image_id': image_id,
+                'PredictionString': format_prediction_string(boxes, scores)
+            }
 
-sample = images[1].permute(1,2,0).cpu().numpy()
-boxes = outputs[1]['boxes'].data.cpu().numpy()
-scores = outputs[1]['scores'].data.cpu().numpy()
+            results.append(result)
 
-boxes = boxes[scores >= detection_threshold].astype(np.int32)
+    sample = images[-1].permute(1,2,0).cpu().numpy()
+    boxes = outputs[-1]['boxes'].data.cpu().numpy()
+    scores = outputs[-1]['scores'].data.cpu().numpy()
 
-fig, ax = plt.subplots(1, 1, figsize=(16, 8))
+    boxes = boxes[scores >= detection_threshold].astype(np.int32)
 
-for box in boxes:
-    cv2.rectangle(sample,
-                  (box[0], box[1]),
-                  (box[2], box[3]),
-                  (220, 0, 0), 2)
+    fig, ax = plt.subplots(1, 1, figsize=(16, 8))
 
-ax.set_axis_off()
-ax.imshow(sample)
+    for box in boxes:
+        cv2.rectangle(sample,
+                      (box[0], box[1]),
+                      (box[2], box[3]),
+                      (220, 0, 0), 2)
+
+    ax.set_axis_off()
+    #ax.imshow(sample)
+    savedfilename = 'static/img/pred_' + filename
+    fig.savefig(savedfilename)
+
+
+    return savedfilename
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
